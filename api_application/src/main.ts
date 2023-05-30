68823e1ef0f12ea33928fc667adc9eac79f35319
jeversonjv <jeversontp@gmail.com>
2023-05-29 23:57:48 -0300

    chore: config k8s

--- api_application/src/main.ts
+++ api_application/src/main.ts
@@ -1,11 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3333);
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT') ?? 3012);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
